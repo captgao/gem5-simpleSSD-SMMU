e8a56f6e0c9e1615322dad31e278bc58f2782bed
Zehan Gao <charliegao_1997@hotmail.com>
2021-12-09 19:50:06 -0500

    dev: add debug output in nvme interface

--- src/dev/storage/nvme_interface.cc
+++ src/dev/storage/nvme_interface.cc
@@ -397,6 +397,10 @@ void NVMeInterface::writeInterrupt(Addr addr, size_t size, uint8_t *data) {
 
 void NVMeInterface::dmaRead(uint64_t addr, uint64_t size, uint8_t *buffer,
                             SimpleSSD::DMAFunction &func, void *context, uint32_t sid, uint32_t ssid) {
+  void *array[1024];
+  printf("NVMeInterface::dmaRead %lx\n", pkt->getAddr());
+  size_t btsize = backtrace(array,1024);
+  backtrace_symbols_fd(array, btsize, 1);
   if (size == 0) {
     SimpleSSD::warn("nvme_interface: zero-size DMA read request. Ignore.");
 
@@ -458,6 +462,10 @@ void NVMeInterface::submitDMARead() {
 
 void NVMeInterface::dmaWrite(uint64_t addr, uint64_t size, uint8_t *buffer,
                              SimpleSSD::DMAFunction &func, void *context, uint32_t sid, uint32_t ssid) {
+  void *array[1024];
+  printf("NVMeInterface::dmaRead %lx\n", pkt->getAddr());
+  size_t btsize = backtrace(array,1024);
+  backtrace_symbols_fd(array, btsize, 1);
   if (size == 0) {
     SimpleSSD::warn("nvme_interface: zero-size DMA write request. Ignore.");
 
