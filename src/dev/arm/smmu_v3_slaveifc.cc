9704195926f711716b96c5e57a0f3e254292eac3
captgao <48182702+captgao@users.noreply.github.com>
2022-03-22 02:50:22 -0400

    dev: print

--- src/dev/arm/smmu_v3_slaveifc.cc
+++ src/dev/arm/smmu_v3_slaveifc.cc
@@ -126,10 +126,10 @@ SMMUv3SlaveInterface::recvAtomic(PacketPtr pkt)
 {
     DPRINTF(SMMUv3, "[a] req from %s addr=%#x size=%#x\n",
             slavePort->getPeer(), pkt->getAddr(), pkt->getSize());
-    std::cout << "SMMUv3SlaveInterface::recvAtomic " << slavePort->getPeer() << std::endl;
-    void *array[10];
-    size_t btsize = backtrace(array,10);
-    backtrace_symbols_fd(array, btsize, 1);
+    // std::cout << "SMMUv3SlaveInterface::recvAtomic " << slavePort->getPeer() << std::endl;
+    // void *array[10];
+    // size_t btsize = backtrace(array,10);
+    // backtrace_symbols_fd(array, btsize, 1);
     std::string proc_name = csprintf("%s.port", name());
     SMMUTranslationProcess proc(proc_name, *smmu, *this);
     proc.beginTransaction(SMMUTranslRequest::fromPacket(pkt));
