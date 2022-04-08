7471aaa9f5ba0c520dff70bde65af4f113e0b590
captgao <48182702+captgao@users.noreply.github.com>
2022-04-08 12:24:19 -0400

    mem: remove print

--- src/mem/noncoherent_xbar.cc
+++ src/mem/noncoherent_xbar.cc
@@ -249,7 +249,7 @@ NoncoherentXBar::recvAtomicBackdoor(PacketPtr pkt, PortID slave_port_id,
     DPRINTF(NoncoherentXBar, "recvAtomic: packet src %s addr 0x%x cmd %s\n",
             slavePorts[slave_port_id]->name(), pkt->getAddr(),
             pkt->cmdString());
-    std::cout << "NonCoherentXBar::recvAtomicBackdoor " << slavePorts[slave_port_id]->name() << std::endl;
+    //std::cout << "NonCoherentXBar::recvAtomicBackdoor " << slavePorts[slave_port_id]->name() << std::endl;
     // void *array[10];
     // size_t btsize = backtrace(array,10);
     // backtrace_symbols_fd(array, btsize, 1);
@@ -268,7 +268,7 @@ NoncoherentXBar::recvAtomicBackdoor(PacketPtr pkt, PortID slave_port_id,
     auto master = masterPorts[master_port_id];
     Tick response_latency = backdoor ?
         master->sendAtomicBackdoor(pkt, *backdoor) : master->sendAtomic(pkt);
-    std::cout << "Master " << master->name() << " Peer " << master->getPeer() << std::endl;
+    //std::cout << "Master " << master->name() << " Peer " << master->getPeer() << std::endl;
     // add the response data
     if (pkt->isResponse()) {
         pkt_size = pkt->hasData() ? pkt->getSize() : 0;
