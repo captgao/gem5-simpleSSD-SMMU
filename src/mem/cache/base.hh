1ec81f8e37d0069b9f1b80df3f234da5e7a9cffb
captgao <48182702+captgao@users.noreply.github.com>
2022-04-10 23:37:55 -0400

    mem: print

--- src/mem/cache/base.hh
+++ src/mem/cache/base.hh
@@ -1120,7 +1120,7 @@ class BaseCache : public ClockedObject
     {
         // should only see writes or clean evicts here
         assert(pkt->isWrite() || pkt->cmd == MemCmd::CleanEvict);
-
+        std::cout << "allocateWriteBuffer " << pkt->req->coreId << std::endl; 
         Addr blk_addr = pkt->getBlockAddr(blkSize);
 
         // If using compression, on evictions the block is decompressed and
